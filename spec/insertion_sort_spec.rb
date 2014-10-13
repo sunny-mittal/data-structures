623c018068a3e6b5d41c2931304b5de6d8475707
Sunny Mittal <sunnymittal2003@gmail.com>
2014-10-13 17:04:54 -0600

    Add benchmark to tests

--- spec/insertion_sort_spec.rb
+++ spec/insertion_sort_spec.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'insertion_sort'
+require 'benchmark'
 
 describe 'Insertion sort method' do
   it 'should properly sort the input array' do
@@ -11,5 +12,14 @@ describe 'Insertion sort method' do
     [3,1,3,4,4].insertion_sort.must_equal [1,3,3,4,4]
     [1,5,4,2,6,7].insertion_sort.must_equal [1,2,4,5,6,7]
     [3,8,9,14,2].insertion_sort.must_equal [2,3,8,9,14]
+  end
+
+  it 'is benchmarked for awesomeness' do
+    puts 'Best'
+    puts Benchmark.measure { (1..10000).to_a.insertion_sort }
+    puts 'Worst'
+    puts Benchmark.measure { 10000.downto(1).to_a.insertion_sort }
+    puts 'Random'
+    puts Benchmark.measure { (1..10000).to_a.shuffle.insertion_sort }
   end
 end
