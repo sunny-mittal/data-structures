6265c5e9bc92ff9237c5bd886fdb0246f4ac2058
Sunny Mittal <sunnymittal2003@gmail.com>
2014-10-18 14:19:54 -0600

    Greatly improve quick-sort

--- lib/quick_sort.rb
+++ lib/quick_sort.rb
@@ -2,30 +2,31 @@ require 'pry'
 require 'pry-nav'
 
 class Array
-  def quick_sort
-    return self if length < 2
+  def qs_partition(left, right, pivot_index)
+    pivot_value = at(pivot_index)
+    self[pivot_index], self[right] = at(right), at(pivot_index)
+    store_index = left
 
-    p_index = rand(length)
-    c_index = 0
-
-    until c_index == length
-      if at(c_index) >= at(p_index) && c_index < p_index
-        swap(c_index, p_index)
-        p_index = c_index
-      elsif at(c_index) <= at(p_index) && c_index > p_index
-        swap(c_index, p_index)
-        p_index, c_index = c_index, p_index
+    (left..right - 1).each do |index|
+      if at(index) < pivot_value
+        self[index], self[store_index] = at(store_index), at(index)
+        store_index += 1
       end
-      c_index += 1
     end
-    left, right = slice(0...p_index), slice(p_index + 1..-1)
-    left, right = left.quick_sort, right.quick_sort
-    left.push(at(p_index)) + right
+
+    self[store_index], self[right] = at(right), at(store_index)
+
+    store_index
   end
 
-  private
+  def quick_sort(left = 0, right = length - 1)
+    if left < right
+      pivot_index = left + (right - left) / 2
+      new_pivot_index = qs_partition(left, right, pivot_index)
+      quick_sort(left, new_pivot_index - 1)
+      quick_sort(new_pivot_index + 1, right)
+    end
 
-  def swap(c_index, p_index)
-    self[c_index], self[p_index] = at(p_index), at(c_index)
+    self
   end
 end
