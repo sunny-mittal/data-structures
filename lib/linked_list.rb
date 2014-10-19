5dfa88650dcdb2cfbdf5e2423efd741f42076e22
Sunny Mittal <sunnymittal2003@gmail.com>
2014-10-19 17:24:46 -0600

    Finish linked list

---
+++ lib/linked_list.rb
@@ -0,0 +1,67 @@
+require 'pry'
+require 'pry-nav'
+
+class Node
+  attr_reader :value
+  attr_accessor :next_node
+
+  def initialize(value)
+    @value = value
+  end
+end
+
+class List
+  attr_reader :size, :head
+
+  def initialize
+    @size = 0
+  end
+
+  def insert(node)
+    node.next_node = @head unless @head.nil?
+    @head = node
+    @size += 1
+  end
+
+  def search(value)
+    return if (node = @head).nil?
+    until node.nil?
+      return node if node.value == value
+      node = node.next_node
+    end
+    node
+  end
+
+  def to_s
+    return '' if (node = @head).nil?
+    result = []
+    until node.nil?
+      result << case node.value
+                when Numeric then node.value
+                when Symbol then ":#{node.value}"
+                else "'#{node.value}'"
+                end
+      node = node.next_node
+    end
+    result.join(', ')
+  end
+
+  def remove(node)
+    return if (current_node = @head).nil? || node.nil?
+    if current_node.value == node.value
+      @head = current_node.next_node
+      @size -= 1
+      return current_node.value
+    else
+      until current_node.nil?
+        previous_node, current_node = current_node, current_node.next_node
+        return if current_node.nil?
+        if current_node.value == node.value
+          previous_node.next_node = current_node.next_node
+          @size -= 1
+          return current_node.value
+        end
+      end
+    end
+  end
+end
---
+++ spec/linked_list_spec.rb
@@ -0,0 +1,43 @@
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'linked_list'
+
+describe 'Linked list' do
+  before { @ll = List.new }
+
+  it 'allows new nodes to be inserted' do
+    previous_length = @ll.size
+    @ll.insert(Node.new('lizard'))
+    @ll.size.must_equal previous_length + 1
+  end
+
+  it 'reports the correct length' do
+    @ll.insert(Node.new('Giraffe'))
+    @ll.size.must_equal 1
+  end
+
+  it 'can find a desired node' do
+    node = Node.new(4)
+    @ll.insert(node)
+    @ll.search(4).must_equal node
+    @ll.search('not there').must_equal nil
+  end
+
+  it 'prints to a csv string' do
+    @ll.insert(Node.new('Lizard'))
+    @ll.insert(Node.new(:giraffe))
+    @ll.insert(Node.new(4))
+    @ll.to_s.must_equal "4, :giraffe, 'Lizard'"
+  end
+
+  it 'allows nodes to be removed' do
+    @ll.insert(Node.new('Lizard'))
+    @ll.insert(Node.new(:giraffe))
+    existing_node = @ll.search('Lizard')
+    previous_length = @ll.size
+    @ll.remove(existing_node).must_equal 'Lizard'
+    @ll.size.must_equal previous_length - 1
+    @ll.search('Lizard').must_equal nil
+    @ll.remove(Node.new(42)).must_equal nil
+  end
+end
