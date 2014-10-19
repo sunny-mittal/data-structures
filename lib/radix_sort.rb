f2167d3df42d32e96fdc790d8cc4ee0dc8c9e27d
Sunny Mittal <sunnymittal2003@gmail.com>
2014-10-18 18:38:01 -0600

    Add support for negative numbers

--- lib/radix_sort.rb
+++ lib/radix_sort.rb
@@ -2,15 +2,21 @@ require 'pry'
 require 'pry-nav'
 
 class Array
-  def radix_sort(pass = 1)
+  def radix_sort
+    negatives = select { |x| x < 0 }
+    positives = select { |x| x >= 0 }
+    negatives.rs_helper.reverse + positives.rs_helper
+  end
+
+  def rs_helper(pass = 1)
     completed = []
     buckets = Array.new(10) { [] }
     each do |num|
-      which_bucket = num.to_s[-pass]
+      which_bucket = num.to_s[-pass] == '-' ? nil : num.to_s[-pass]
       which_bucket ? buckets[which_bucket.to_i] << num : completed << num
     end
     buckets.flatten!
-    completed += buckets.radix_sort(pass += 1) unless buckets.empty?
+    completed += buckets.rs_helper(pass += 1) unless buckets.empty?
     completed
   end
 end
